ed8830b1e1e1f263a6e878bcd0478c38e3ce9b21
Starovoytov Alexey <relz0071@gmail.com>
2018-04-10 21:53:59 +0300

    Рефакторинг

--- Input/Constant.h
+++
@@ -1,8 +0,0 @@
-#ifndef LANGUAGETRANSLATOR_CONSTANT_H
-#define LANGUAGETRANSLATOR_CONSTANT_H
-
-#include <limits>
-
-static const char NOT_A_CHARACTER = std::numeric_limits<char>::max();
-
-#endif //LANGUAGETRANSLATOR_CONSTANT_H
